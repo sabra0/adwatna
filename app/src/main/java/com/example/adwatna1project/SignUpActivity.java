3aa7b1050d54a5268467c77d0fa46b659ecdd4d8
kcs <sabramahmoud761@gmail.com>
2019-12-08 21:31:01 +0200

    last modification in login and sign up screens

--- app/src/main/java/com/example/adwatna1project/SignUpActivity.java
+++ app/src/main/java/com/example/adwatna1project/SignUpActivity.java
@@ -4,21 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    EditText editText;
     Button button ;
+    RadioButton btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-
         button=findViewById(R.id.back2_btn);
+        editText= findViewById(R.id.gender);
+        radioGroup=findViewById(R.id.gender_layout);
 
+
+        //to prevent user from writing in gender EditText
+        editText.setFocusable(false);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioGroup.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // to make male and female button disappear after choosing one
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.btn1||checkedId==R.id.btn2){
+
+                    radioGroup.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // button to get back
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
