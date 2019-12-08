515d1389edab4df6349d386ec90a0015048f6732
kcs <sabramahmoud761@gmail.com>
2019-12-08 18:41:16 +0200

    last modification in login and sign up screens

--- app/src/main/java/com/example/adwatna1project/MainActivity.java
+++ app/src/main/java/com/example/adwatna1project/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-private static int SPLASH_TIME_OUT=4000;
+private static int SPLASH_TIME_OUT=4000; //time of splash screen: 4 seconds
 ImageView gif ;
 private ImageView logo;
 
