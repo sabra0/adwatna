c2b55a38c3a4609ac38da404ae45736635fbdaa9
kcs <sabramahmoud761@gmail.com>
2020-01-22 05:16:25 +0200

    last modification in login and sign up screens

---
+++ app/src/main/java/com/example/adwatna1project/ArtCategoryFragment.java
@@ -0,0 +1,66 @@
+package com.example.adwatna1project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ArtCategoryFragment extends Fragment {
+    static RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference allDataReference;
+
+    public ArtCategoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView= inflater.inflate(R.layout.fragment_art_category, container, false);
+
+        //recycler view
+        mRecyclerView=rootView.findViewById(R.id.art_category_rv);
+        // mRecyclerView.setHasFixedSize(true);
+
+        int numberOfColumns =3;
+        //set layout as grid layout
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),numberOfColumns));
+
+        //send query to fireBase database
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        allDataReference=mFirebaseDatabase.getReference("Data2").child("art");
+
+        return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Model, ViewHolder>(
+                        Model.class,
+                        R.layout.row_item,
+                        ViewHolder.class,
+                        allDataReference
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Model model, int i) {
+
+                        viewHolder.setDetails(getActivity(),model.getTitle(),model.getPrice(),model.getImage());
+                    }
+                };
+        //set adapter to recyclerView
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+}
--- app/src/main/java/com/example/adwatna1project/WelcomePage.java
+++ app/src/main/java/com/example/adwatna1project/WelcomePage.java
@@ -1,19 +1,12 @@
 package com.example.adwatna1project;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.transition.Visibility;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
@@ -22,12 +15,13 @@ import com.google.firebase.database.Query;
 
 public class WelcomePage extends AppCompatActivity {
 
-    RecyclerView mRecyclerView;
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference allDataReference;
     ImageView searchImageView,backImageView;
     EditText editText;
     String text;
+    TextView allCategories,engineeringCategory,artCategory,electronicsCategory,
+            booksCategory, devicesCategory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +30,16 @@ public class WelcomePage extends AppCompatActivity {
         editText =findViewById(R.id.search_text);
         searchImageView=findViewById(R.id.search_btn);
         backImageView=findViewById(R.id.back_to_all_btn);
+        allCategories=findViewById(R.id.all_categories);
+        engineeringCategory=findViewById(R.id.engineering_category);
+        artCategory=findViewById(R.id.art_category);
+        electronicsCategory=findViewById(R.id.electronics_category);
+        booksCategory=findViewById(R.id.books_category);
+        devicesCategory = findViewById(R.id.devices_category);
 
-        //recycler view
-        mRecyclerView=findViewById(R.id.recyclerView);
-       // mRecyclerView.setHasFixedSize(true);
-
-        int numberOfColumns =3;
-        //set layout as linear layout
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this,numberOfColumns));
+        //to open allCategories fragment by default
+        getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new AllCategoriesFragment()).commit();
+        allCategories.setTextColor(getResources().getColor(R.color.colorAccent));
 
         //send query to fireBase database
         mFirebaseDatabase = FirebaseDatabase.getInstance();
@@ -65,6 +61,7 @@ public class WelcomePage extends AppCompatActivity {
                     fireBaseSearch(text);
                     editText.setVisibility(View.GONE);
                     backImageView.setVisibility(View.VISIBLE);
+
                 }
             }
         });
@@ -76,6 +73,97 @@ public class WelcomePage extends AppCompatActivity {
                 backImageView.setVisibility(View.GONE);
             }
         });
+
+        //for all categories fragment
+        allCategories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                allCategories.setTextColor(getResources().getColor(R.color.colorAccent));
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new AllCategoriesFragment()).commit();
+
+            }
+        });
+
+        engineeringCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorAccent));
+                allCategories.setTextColor(getResources().getColor(R.color.colorBlack));
+                artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new EngineeringCatigoryFragment()).commit();
+
+            }
+        });
+
+        artCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                artCategory.setTextColor(getResources().getColor(R.color.colorAccent));
+                allCategories.setTextColor(getResources().getColor(R.color.colorBlack));
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new ArtCategoryFragment()).commit();
+
+            }
+        });
+
+        electronicsCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorAccent));
+                allCategories.setTextColor(getResources().getColor(R.color.colorBlack));
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new ElectronicsCategoryFragment()).commit();
+
+            }
+        });
+        booksCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                booksCategory.setTextColor(getResources().getColor(R.color.colorAccent));
+                allCategories.setTextColor(getResources().getColor(R.color.colorBlack));
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new BooksCategoryFragment()).commit();
+
+            }
+        });
+
+        devicesCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                devicesCategory.setTextColor(getResources().getColor(R.color.colorAccent));
+                allCategories.setTextColor(getResources().getColor(R.color.colorBlack));
+                engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+                artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new DevicesCategoryFragment()).commit();
+
+            }
+        });
+
     }
     //search function
     private void fireBaseSearch (String searchText){
@@ -95,31 +183,21 @@ public class WelcomePage extends AppCompatActivity {
                         viewHolder.setDetails(getApplicationContext(),model.getTitle(),model.getPrice(),model.getImage());
                     }
                 };
+       /* //open all categories fragment after search
+        getSupportFragmentManager().beginTransaction().replace(R.id.welcome_page_frame,new AllCategoriesFragment()).commit();
+        //set  horizontal scroll view colors
+        allCategories.setTextColor(getResources().getColor(R.color.colorAccent));
+        engineeringCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+        artCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+        electronicsCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+        booksCategory.setTextColor(getResources().getColor(R.color.colorBlack));
+        devicesCategory.setTextColor(getResources().getColor(R.color.colorBlack));*/
+
         //set adapter to recyclerView
-        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+        AllCategoriesFragment.mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+
     }
 
-    //load data into recycler view on start
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Model, ViewHolder>(
-                        Model.class,
-                        R.layout.row_item,
-                        ViewHolder.class,
-                        allDataReference
-                ) {
-                    @Override
-                    protected void populateViewHolder(ViewHolder viewHolder, Model model, int i) {
-
-                        viewHolder.setDetails(getApplicationContext(),model.getTitle(),model.getPrice(),model.getImage());
-                    }
-                };
-        //set adapter to recyclerView
-        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
-
-    }
 // for actionBar to make search button and options menu
 
     /*@Override
